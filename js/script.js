39cbc57c6ca8450a70a01c51078648562fcde2f2
Elena Kud <elenakudmax@gmail.com>
2024-06-12 11:46:54 +0300

    made changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 new Swiper('.swiper', {
+  slidesPerView: 1,
+  speed: 1000,
   navigation: {
     nextEl: '.portfolio__arrow_right',
     prevEl: '.portfolio__arrow_left',
     disabledClass: 'portfolio__arrow_disable',
   },
-})
+  mousewheel: true,
+  keyboard: true,
+});
+
+
 
 $('.header__contacts-burger').click(() => {
   $('.header__contacts').slideToggle();
@@ -23,3 +29,9 @@ $('.page__overlay').click((event) => {
     $('.page__overlay_modal').fadeOut(400);
   }
 })
+
+
+const inputTel = document.querySelector('.callback__input_tel');    
+const telMask = new Inputmask('+375(99)999-99-99'); 
+
+telMask.mask(inputTel);
